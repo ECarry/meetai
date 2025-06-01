446a440f0cf1604e9390ed4b005fc6eeb14e44de
ecarry <lianshiliang93@gmail.com>
2025-06-01 15:26:44 +0000

    feat: replace Geist with Inter font and implement responsive navbar with search

--- src/modules/dashboard/ui/components/dashboard-navbar.tsx
+++ src/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -1,5 +1,35 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { PanelLeft, PanelLeftClose, Search } from "lucide-react";
 import React from "react";
+import { useSidebar } from "@/components/ui/sidebar";
 
 export const DashboardNavbar = () => {
-  return <div>DashboardNavbar</div>;
+  const { toggleSidebar, state, isMobile } = useSidebar();
+
+  return (
+    <div className="flex px-4 gap-x-2 items-center py-3 border-b bg-background">
+      <Button className="size-9" variant="outline" onClick={toggleSidebar}>
+        {state === "collapsed" || isMobile ? (
+          <PanelLeft className="size-4" />
+        ) : (
+          <PanelLeftClose className="size-4" />
+        )}
+      </Button>
+
+      <Button
+        className="h-9 w-[240px] justify-start font-normal text-muted-foreground hover:text-muted-foreground"
+        variant="outline"
+        size="sm"
+        onClick={() => {}}
+      >
+        <Search />
+
+        <kbd className="ml-auto pointer-events-none inline-flex items-center gap-1 select-none rounded border px-1.5 font-mono bg-muted text-[10px] text-muted-foreground">
+          <span className="text-xs">&#8984;</span>K
+        </kbd>
+      </Button>
+    </div>
+  );
 };
