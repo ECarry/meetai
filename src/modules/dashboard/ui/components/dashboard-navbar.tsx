3b371d55b00732175bc8f733602286484db6c773
ecarry <lianshiliang93@gmail.com>
2025-06-01 15:40:44 +0000

    feat: add command palette with keyboard shortcut support

--- src/modules/dashboard/ui/components/dashboard-navbar.tsx
+++ src/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -1,35 +1,56 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { PanelLeft, PanelLeftClose, Search } from "lucide-react";
-import React from "react";
 import { useSidebar } from "@/components/ui/sidebar";
+import { DashboardCommand } from "./dashboard-command";
 
 export const DashboardNavbar = () => {
   const { toggleSidebar, state, isMobile } = useSidebar();
 
+  const [commandOpen, setCommandOpen] = useState(false);
+
+  useEffect(() => {
+    const keydown = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setCommandOpen((prev) => !prev);
+      }
+    };
+
+    document.addEventListener("keydown", keydown);
+
+    return () => {
+      document.removeEventListener("keydown", keydown);
+    };
+  }, []);
+
   return (
-    <div className="flex px-4 gap-x-2 items-center py-3 border-b bg-background">
-      <Button className="size-9" variant="outline" onClick={toggleSidebar}>
-        {state === "collapsed" || isMobile ? (
-          <PanelLeft className="size-4" />
-        ) : (
-          <PanelLeftClose className="size-4" />
-        )}
-      </Button>
+    <>
+      <DashboardCommand open={commandOpen} setOpen={setCommandOpen} />
+      <nav className="flex px-4 gap-x-2 items-center py-3 border-b bg-background">
+        <Button className="size-9" variant="outline" onClick={toggleSidebar}>
+          {state === "collapsed" || isMobile ? (
+            <PanelLeft className="size-4" />
+          ) : (
+            <PanelLeftClose className="size-4" />
+          )}
+        </Button>
 
-      <Button
-        className="h-9 w-[240px] justify-start font-normal text-muted-foreground hover:text-muted-foreground"
-        variant="outline"
-        size="sm"
-        onClick={() => {}}
-      >
-        <Search />
+        <Button
+          className="h-9 w-[240px] justify-start font-normal text-muted-foreground hover:text-muted-foreground"
+          variant="outline"
+          size="sm"
+          onClick={() => setCommandOpen(true)}
+        >
+          <Search />
 
-        <kbd className="ml-auto pointer-events-none inline-flex items-center gap-1 select-none rounded border px-1.5 font-mono bg-muted text-[10px] text-muted-foreground">
-          <span className="text-xs">&#8984;</span>K
-        </kbd>
-      </Button>
-    </div>
+          <kbd className="ml-auto pointer-events-none inline-flex items-center gap-1 select-none rounded border px-1.5 font-mono bg-muted text-[10px] text-muted-foreground">
+            <span className="text-xs">&#8984;</span>K
+          </kbd>
+        </Button>
+      </nav>
+    </>
   );
 };
