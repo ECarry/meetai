ee2d60736feace55dca69740a4abf8ba760ae3b7
ecarry <lianshiliang93@gmail.com>
2025-06-02 15:02:36 +0000

    feat: integrate tRPC with Next.js app router and add hello query endpoint

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export const HomeView = () => {
-  return (
-    <div>
-      <Button>ffff</Button>
-    </div>
-  );
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "world123" }));
+
+  return <div>{data?.greeting}</div>;
 };
